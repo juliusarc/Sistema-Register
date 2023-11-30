038d1780644114b6cafb344a33165b5caee649bb
juliusarc <julius.cgm@gmail.com>
2023-11-30 15:10:26 -0300

    Add project files.

---
+++ Biblitec/Bibli1.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Bibli1.aspx.cs" Inherits="Biblitec.Bibli1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Informações do Livro</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        form {
+            max-width: 400px;
+            margin: 20px auto;
+            background: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h2 {
+            text-align: center;
+            color: #333;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+            color: #555;
+        }
+
+        input[type="text"] {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 10px;
+            box-sizing: border-box;
+        }
+
+        button {
+            background-color: #4caf50;
+            color: #fff;
+            padding: 10px;
+            border: none;
+            border-radius: 3px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #45a049;
+        }
+
+        #lblResult {
+            margin-top: 10px;
+            color: #333;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Informações do Livro</h2>
+            <asp:Label ID="lblResult" runat="server" EnableViewState="false"></asp:Label>
+            <asp:Label ID="lblTitle" runat="server" Text="Título do Livro:"></asp:Label>
+            <asp:TextBox ID="txtTitle" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="lblAuthor" runat="server" Text="Autor:"></asp:Label>
+            <asp:TextBox ID="txtAuthor" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="lblYear" runat="server" Text="Ano de Lançamento:"></asp:Label>
+            <br />
+            
+            <asp:TextBox ID="txtYear" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="lblId" runat="server" Text="ID:"></asp:Label>
+            <asp:TextBox ID="txtId" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnSubmit" runat="server" Text="Salvar Livro" OnClick="btnSubmit_Click" />
+            <asp:Button ID="btnUpdate" runat="server" Text="Atualizar Livro" OnClick="btnUpdate_Click" />
+            <asp:Button ID="btnDelete" runat="server" Text="Excluir Livro" OnClick="btnDelete_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Biblitec/Bibli1.aspx.cs
@@ -0,0 +1,138 @@
+﻿using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Biblitec
+{
+    public partial class Bibli1 : Page
+    {
+        // Substitua a string de conexão com os detalhes do seu banco de dados MySQL
+        string connectionString = "server=localhost;uid=root;pwd=ghoull18;database=Biblib";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CarregarDados();
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            SalvarLivro();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            AtualizarLivro();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            ExcluirLivro();
+        }
+
+        private void CarregarDados()
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Livros";
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            // Aqui você pode carregar os dados em um controle, como um GridView
+                            // Exemplo: gridViewLivros.DataSource = reader; gridViewLivros.DataBind();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = $"Erro ao carregar dados: {ex.Message}";
+            }
+        }
+
+        private void SalvarLivro()
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "INSERT INTO Livros (Titulo, Autor, AnoLancamento) VALUES (@Titulo, @Autor, @AnoLancamento)";
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Titulo", txtTitle.Text);
+                        command.Parameters.AddWithValue("@Autor", txtAuthor.Text);
+                        command.Parameters.AddWithValue("@AnoLancamento", Convert.ToInt32(txtYear.Text));
+                        command.ExecuteNonQuery();
+                    }
+
+                    lblResult.Text = "Livro salvo com sucesso!";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = $"Erro ao salvar livro: {ex.Message}";
+            }
+        }
+
+        private void AtualizarLivro()
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "UPDATE Livros SET Titulo = @Titulo, Autor = @Autor, AnoLancamento = @AnoLancamento WHERE Id = @Id";
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Titulo", txtTitle.Text);
+                        command.Parameters.AddWithValue("@Autor", txtAuthor.Text);
+                        command.Parameters.AddWithValue("@AnoLancamento", Convert.ToInt32(txtYear.Text));
+                        command.Parameters.AddWithValue("@Id", Convert.ToInt32(txtId.Text));
+                        command.ExecuteNonQuery();
+                    }
+
+                    lblResult.Text = "Livro atualizado com sucesso!";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = $"Erro ao atualizar livro: {ex.Message}";
+            }
+        }
+
+        private void ExcluirLivro()
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "DELETE FROM Livros WHERE Id = @Id";
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id", Convert.ToInt32(txtId.Text));
+                        command.ExecuteNonQuery();
+                    }
+
+                    lblResult.Text = "Livro excluído com sucesso!";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = $"Erro ao excluir livro: {ex.Message}";
+            }
+        }
+    }
+}
